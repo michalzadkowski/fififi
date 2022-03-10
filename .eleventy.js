490611a608cd4a57d9e155e0e010c9d83dd22090
Michał Żadkowski <michal.zadkowski@getresponse.com>
2022-03-10 15:19:57 +0100

    passthrougg logos

--- .eleventy.js
+++ .eleventy.js
@@ -48,6 +48,7 @@ module.exports = function (eleventyConfig) {
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
   eleventyConfig.addPassthroughCopy("./src/static/files");
+  eleventyConfig.addPassthroughCopy("./src/static/logos");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
